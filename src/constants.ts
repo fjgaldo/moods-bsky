f460ed595feaa27882eaa02769454886ab612c00
Francisco J. Galdo <fjgaldo@users.noreply.github.com>
2024-08-29 11:39:36 +0200

    Adapt constants.ts and add first emojis

--- src/constants.ts
+++ src/constants.ts
@@ -1,160 +1,17 @@
 import "dotenv/config";
 
-export const PRONOUNS: Record<string, string> = {
-  "she/her": "she",
-  "he/him": "he",
-  "they/them": "they",
-  "it/its": "it",
-  "any/all": "any",
-  ask: "ask",
-  avoid: "avoid",
-  "look at bio": "bio",
-  "fae/faer": "fae",
-  "bun/buns": "bun",
-  "xe/xir": "xe",
-  "drae/draer": "drae",
-  "e/em/eir": "e",
-  "sier/siehn/siere": "sier",
-  "ey/em/eir": "ey",
-  "shey/sheir/sher": "shey",
-  "voi/void": "voi",
-  "mew/mews": "mew",
-  "paw/paws": "paw",
-  "kit/kits": "kit",
-  "zie/zir": "zie",
-  "ae/aer": "ae",
-  "prince/princes/princeself": "prince",
-  "ze/hir": "ze",
-  "dey/deren": "dey",
-  none: "none",
-  "e/em/er": "e-em-er",
-  "xier/xies": "xier",
-  "sier/sies": "sier-sies",
-  "ens/ens": "ens",
-  "squee/squir": "squee",
-  "vi/vix": "vi",
-  "ne/nem/nir": "ne-nem-nir",
-  "ze/zim": "ze-zim",
-  "pup/pups/pupself": "pup-pups-pupself",
-  "zi/zir": "zi-zir",
-  "he/haw": "he-haw",
-  "hee/haw": "hee-haw",
-  "zhi/zhr": "zhi-zhr",
-  "ve/ver": "ve-ver",
-  "sí/hir": "si-hir",
-  "she/we": "she-we",
-  "he/we": "he-we",
-  "they/we": "they-we",
-  "xe/xem": "xe-xem",
-  "sun/suns": "sun-suns",
-  "leaf/leafs": "leaf-leafs",
-  "shiv/ver": "shiv-ver",
-  "drüsshe/drayr": "drusshe-drayr",
+export const MOODS: Record<string, string> = {
+  "Grinning Face": "grinning-face",
+  "Smirking Face": "smirking-face",
 };
 
 export const URIs: Record<string, string> = {
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwsqucto3j2a":
+  "at://did:plc:4s3i2xuwzggf4zqknkq7f53a/app.bsky.feed.post/3l2tmnobwkl2r":
     "Like this post to delete your labels",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwss4ldkwd2j":
-    "they/them",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwss4fmiow2n":
-    "it/its",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwss4bzqlw2k":
-    "he/him",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwss45mxrh2j":
-    "she/her",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwst2tn2342f":
-    "look at bio",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwsslg3gqk2t":
-    "avoid",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwssldhzme27":
-    "ask",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwss4vc4cw2x":
-    "any/all",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwu55hxj4b2a":
-    "fae/faer",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwu5d6rq352v":
-    "bun/buns",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwu5s4lt6f2k":
-    "xe/xir",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwu5sjyg4m2f":
-    "drae/draer",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwu5stootb2q":
-    "e/em/eir",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwu5taglmr2q":
-    "sier/siehn/siere",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwu6b3cnd52k":
-    "ey/em/eir",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwu7n26cgm2m":
-    "shey/sheir/sher",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwue4lqwjb2q":
-    "voi/void",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwueewr2p22j":
-    "mew/mews",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwukwwz4nt2j":
-    "paw/paws",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwuv4vxgwu2n":
-    "kit/kits",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwuv56n4jv2g":
-    "zie/zir",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwuvaelsru2m":
-    "ae/aer",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwv2d6oguw2n":
-    "prince/princes/princeself",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwvcc736wg2n":
-    "ze/hir",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwwwvsvcnz2h":
-    "dey/deren",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwxljuxooh2r":
-    "none",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwzimwll6j2q":
-    "e/em/er",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwzmcaue3e22":
-    "xier/xies",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwzmclqnud2t":
-    "sier/sies",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kx22bkmnco2t":
-    "ens/ens",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kx45q2fq372f":
-    "squee/squir",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kx4puqpu5q2j":
-    "vi/vix",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kxntgh45yy2j":
-    "ne/nem/nir",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kxqmnktb532k":
-    "ze/zim",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kxsppngl4e2a":
-    "pup/pups/pupself",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kxvjiundpz2r":
-    "zi/zir",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kxvjixhmhh2g":
-    "he/haw",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kxwujilwty2m":
-    "hee/haw",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kxy7b27dke2w":
-    "zhi/zhr",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3ky7xcoqkza2q":
-    "ve/ver",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kygr2mke2t2z":
-    "sí/hir",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kyikdvmeff2g":
-    "she/we",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kyikdz4eau2a":
-    "he/we",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kyike4fibu2k":
-    "they/we",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kyilq67fyv2a":
-    "xe/xem",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kzfbgd3jpz2k":
-    "sun/suns",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kzfbgi7is225":
-    "leaf/leafs",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kzftskynd32l":
-    "shiv/ver",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3l2jb2koecj27":
-    "ferret/ferrets/ferretself",
-  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3l2kx6qwefk2h":
-    "drüsshe/drayr",
+  "at://did:plc:4s3i2xuwzggf4zqknkq7f53a/app.bsky.feed.post/3l2tmub7ook2v":
+    "Grinning Face",
+  "at://did:plc:4s3i2xuwzggf4zqknkq7f53a/app.bsky.feed.post/3l2tmtwcltn22":
+    "Smirking Face",
 };
 
 export const DID = process.env.DID ?? "";
