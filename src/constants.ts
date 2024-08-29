8d73cd6b5980c28c3551ed87a37141153d6b6600
Francisco J. Galdo <fjgaldo@users.noreply.github.com>
2024-08-29 15:30:26 +0200

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,17 +1,17 @@
 import "dotenv/config";
 
 export const MOODS: Record<string, string> = {
+  "Beaming Face with Smiling Eyes": "beaming-face-with-smiling-eyes",
   "Grinning Face": "grinning-face",
-  "Smirking Face": "smirking-face",
 };
 
 export const URIs: Record<string, string> = {
-  "at://did:plc:4s3i2xuwzggf4zqknkq7f53a/app.bsky.feed.post/3l2tmnobwkl2r":
+  "at://did:plc:rh2ofunvtpc3hfilipjmwxm5/app.bsky.feed.post/3l2uae6ynpv25":
     "Like this post to delete your labels",
-  "at://did:plc:4s3i2xuwzggf4zqknkq7f53a/app.bsky.feed.post/3l2tmub7ook2v":
+  "at://did:plc:rh2ofunvtpc3hfilipjmwxm5/app.bsky.feed.post/3l2uczbg6hc2l":
     "Grinning Face",
-  "at://did:plc:4s3i2xuwzggf4zqknkq7f53a/app.bsky.feed.post/3l2tmtwcltn22":
-    "Smirking Face",
+  "at://did:plc:rh2ofunvtpc3hfilipjmwxm5/app.bsky.feed.post/3l2ucyg4wz22h":
+    "Beaming Face with Smiling Eyes",
 };
 
 export const DID = process.env.DID ?? "";
