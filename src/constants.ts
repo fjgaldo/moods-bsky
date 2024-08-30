c73b9fe75c41607422778750dd5ebf6277f79879
Francisco J. Galdo <fjgaldo@users.noreply.github.com>
2024-08-30 15:32:10 +0200

    First batch of emojis

--- src/constants.ts
+++ src/constants.ts
@@ -3,15 +3,86 @@ import "dotenv/config";
 export const MOODS: Record<string, string> = {
   "Beaming Face with Smiling Eyes": "beaming-face-with-smiling-eyes",
   "Grinning Face": "grinning-face",
+  "Flushed Face": "flushed-face",
+  "Smiling Face with Hearts": "smiling-face-with-hearts",
+  "Exploding Head": "exploding-head",
+  "Face with Stream from Nose": "face-with-stream-from-nose",
+  "Pleading Face": "pleading-face",
+  "Partying Face": "partying-face",
+  "Loudly Crying Face": "loudly-crying-face",
+  "Winking Face with Tongue": "winking-face-with-tongue",
+  "Smiling Face with Sunglasses": "smiling-face-with-sunglasses",
+  "Thinking Face": "thinking-face",
+  "Sleeping Face": "sleeping-face",
+  "Face Screaming in Fear": "face-screaming-in-fear",
+  "Pouting Face": "pouting-face",
+  "Crying Face": "crying-face",
+  "Confused Face": "confused-face",
+  "Pensive Face": "pensive-face",
+  "Smirking Face": "smirking-face",
+  "Face Blowing a Kiss": "face-blowing-a-kiss",
+  "Star Struck": "star-struck",
+  "Smiling Face with Heart Shaped Eyes": "smiling-face-with-heart-shaped-eyes",
+  "Smiling Face with Halo": "smiling-face-with-halo",
+  "Smiling Face with Smiling Eyes": "smiling-face-with-smiling-eyes",
+  "Face with Tears of Joy": "face-with-tears-of-joy",
 };
 
 export const URIs: Record<string, string> = {
   "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wall4qyd2r":
     "Like this post to delete your labels",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2waptq3n422":
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wrqx57mx2p":
     "Grinning Face",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2waqh4n6622":
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtqv6hbc2o":
+    "Flushed Face",
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtqjnig627":
+    "Smiling Face with Hearts",
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtpyqqum2s":
+    "Exploding Head",
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtphjryh22":
+    "Face with Stream from Nose",
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtoxow5w2n":
+    "Pleading Face",
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtol3m2g2s":
+    "Partying Face",
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wto34u622s":
+    "Loudly Crying Face",
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtnonivl2d":
+    "Winking Face with Tongue",
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtmqxv4w27":
+    "Thinking Face",
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtmat7z326":
+    "Sleeping Face",
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtlgfq672c":
+    "Face Screaming in Fear",
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtkwvuyf2y":
+    "Pouting Face",
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtkfj2ol2u":
+    "Crying Face",
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtjwute62n":
+    "Confused Face",
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtje34fc27":
+    "Pensive Face",
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtiu5wl326":
+    "Smirking Face",
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wrfjghc222":
+    "Face Blowing a Kiss",
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wrez32fs2o":
+    "Star Struck",
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wrejze6322":
+    "Smiling Face with Heart Shaped Eyes",
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wrdzvcho27":
+    "Smiling Face with Halo",
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wrcvjxww2n":
+    "Face with Tears of Joy",
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wrdgnpdg22":
+    "Smiling Face with Smiling Eyes",
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtrd7utb2t":
     "Beaming Face with Smiling Eyes",
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wts2t7oy22":
+    "Grinning Face",
+  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtn55ujv2y":
+    "Smiling Face with Sunglasses",
 };
 
 export const DID = process.env.DID ?? "";
