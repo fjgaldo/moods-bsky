12a03383cb41f980c381e28c0deaa6696d6bc8b3
Francisco J. Galdo <fjgaldo@users.noreply.github.com>
2024-09-02 15:38:18 +0200

    Update constants.ts for new format + new emoji

--- src/constants.ts
+++ src/constants.ts
@@ -1,134 +1,50 @@
 import "dotenv/config";
 
-export const MOODS: Record<string, string> = {
-  "Beaming Face with Smiling Eyes": "beaming-face-with-smiling-eyes",
-  "Grinning Face": "grinning-face",
-  "Flushed Face": "flushed-face",
-  "Smiling Face with Hearts": "smiling-face-with-hearts",
-  "Exploding Head": "exploding-head",
-  "Face with Stream from Nose": "face-with-stream-from-nose",
-  "Pleading Face": "pleading-face",
-  "Partying Face": "partying-face",
-  "Loudly Crying Face": "loudly-crying-face",
-  "Winking Face with Tongue": "winking-face-with-tongue",
-  "Smiling Face with Sunglasses": "smiling-face-with-sunglasses",
-  "Thinking Face": "thinking-face",
-  "Sleeping Face": "sleeping-face",
-  "Face Screaming in Fear": "face-screaming-in-fear",
-  "Pouting Face": "pouting-face",
-  "Crying Face": "crying-face",
-  "Confused Face": "confused-face",
-  "Pensive Face": "pensive-face",
-  "Smirking Face": "smirking-face",
-  "Face Blowing a Kiss": "face-blowing-a-kiss",
-  "Star Struck": "star-struck",
-  "Smiling Face with Heart Eyes": "smiling-face-with-heart-eyes",
-  "Smiling Face with Halo": "smiling-face-with-halo",
-  "Smiling Face with Smiling Eyes": "smiling-face-with-smiling-eyes",
-  "Face with Tears of Joy": "face-with-tears-of-joy",
-  "Face Exhaling": "face-exhaling",
-  "Face with Rolling Eyes": "face-with-rolling-eyes",
-  "Melting Face": "melting-face",
-  "Hole": "hole",
-  "Shrug": "shrug",
-  "Mood Swings": "mood-swings",
-  "Heart Hands": "heart-hands",
-  "Middle Finger": "middle-finger",
-  "Zombie": "zombie",
-  "Bat": "bat",
-  "Blood Drop": "blood-drop",
-  "Skull": "skull",
-  "Ghost": "ghost",
-  "Jack O Lantern": "jack-o-lantern",
-  "Coffe and Cigarette": "coffe-cigarette",
-};
-
-export const URIs: Record<string, string> = {
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wall4qyd2r":
-    "Like this post to delete your labels",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wrqx57mx2p":
-    "Grinning Face",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtqv6hbc2o":
-    "Flushed Face",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtqjnig627":
-    "Smiling Face with Hearts",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtpyqqum2s":
-    "Exploding Head",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtphjryh22":
-    "Face with Stream from Nose",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtoxow5w2n":
-    "Pleading Face",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtol3m2g2s":
-    "Partying Face",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wto34u622s":
-    "Loudly Crying Face",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtnonivl2d":
-    "Winking Face with Tongue",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtmqxv4w27":
-    "Thinking Face",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtmat7z326":
-    "Sleeping Face",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtlgfq672c":
-    "Face Screaming in Fear",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtkwvuyf2y":
-    "Pouting Face",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtkfj2ol2u":
-    "Crying Face",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtjwute62n":
-    "Confused Face",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtje34fc27":
-    "Pensive Face",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtiu5wl326":
-    "Smirking Face",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wrfjghc222":
-    "Face Blowing a Kiss",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wrez32fs2o":
-    "Star Struck",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wrejze6322":
-    "Smiling Face with Heart Eyes",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wrdzvcho27":
-    "Smiling Face with Halo",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wrcvjxww2n":
-    "Face with Tears of Joy",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wrdgnpdg22":
-    "Smiling Face with Smiling Eyes",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtrd7utb2t":
-    "Beaming Face with Smiling Eyes",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wts2t7oy22":
-    "Grinning Face",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wtn55ujv2y":
-    "Smiling Face with Sunglasses",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wvpvetyk22":
-    "Hole",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wvm3rtbt2r":
-    "Face Exhaling",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wvlh6xgg2s":
-    "Face with Rolling Eyes",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2wvkhmq6b2s":
-    "Melting Face",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2zmhoqubq2p":
-    "Mood Swings",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2zmdh4nwg2s":
-    "Shrug",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2zmgnpv7c22":
-    "Heart Hands",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2zmfd7k4e2n":
-    "Middle Finger",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2zrg2r3gu2h":
-    "Zombie",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2zrfmfldy22":
-    "Bat",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2zrfayyxo2p":
-    "Blood Drop",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2zrewoxky2s":
-    "Skull",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2zrek5jzz22":
-    "Ghost",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2zre5lg542s":
-    "Jack O Lantern",
-  "at://did:plc:7m7xz5ekdmw3tephpgn72ooz/app.bsky.feed.post/3l2zrc72g2m2o":
-    "Coffe and Cigarette",
-};
-
 export const DID = process.env.DID ?? "";
 export const SIGNING_KEY = process.env.SIGNING_KEY ?? "";
+export const POSTS: Record<string, string> = {
+  "3l2wall4qyd2r": "delete",
+  "3l2wrqx57mx2p": "grinning-face",
+  "3l2wtqv6hbc2o": "flushed-face",
+  "3l2wtqjnig627": "smiling-face-with-hearts",
+  "3l2wtpyqqum2s": "exploding-head",
+  "3l2wtphjryh22": "face-with-stream-from-nose",
+  "3l2wtoxow5w2n": "pleading-face",
+  "3l2wtol3m2g2s": "partying-face",
+  "3l2wto34u622s": "loudly-crying-face",
+  "3l2wtnonivl2d": "winking-face-with-tongue",
+  "3l2wtmqxv4w27": "thinking-face",
+  "3l2wtmat7z326": "sleeping-face",
+  "3l2wtlgfq672c": "face-screaming-in-fear",
+  "3l2wtkwvuyf2y": "pouting-face",
+  "3l2wtkfj2ol2u": "crying-face",
+  "3l2wtjwute62n": "confused-face",
+  "3l2wtje34fc27": "pensive-face",
+  "3l2wtiu5wl326": "smirking-face",
+  "3l2wrfjghc222": "face-blowing-a-kiss",
+  "3l2wrez32fs2o": "star-struck",
+  "3l2wrejze6322": "smiling-face-with-heart-eyes",
+  "3l2wrdzvcho27": "smiling-face-with-halo",
+  "3l2wrcvjxww2n": "face-with-tears-of-joy",
+  "3l2wrdgnpdg22": "smiling-face-with-smiling-eyes",
+  "3l2wtrd7utb2t": "beaming-face-with-heart-eyes",
+  "3l2wts2t7oy22": "grinning-face",
+  "3l2wtn55ujv2y": "smiling-face-with-sunglasses",
+  "3l2wvpvetyk22": "hole",
+  "3l2wvm3rtbt2r": "face-exhaling",
+  "3l2wvlh6xgg2s": "face-with-rolling-eyes",
+  "3l2wvkhmq6b2s": "melting-face",
+  "3l2zmhoqubq2p": "mood-swings",
+  "3l2zmdh4nwg2s": "shrug",
+  "3l2zmgnpv7c22": "heart-hands",
+  "3l2zmfd7k4e2n": "middle-finger",
+  "3l2zrg2r3gu2h": "zombie",
+  "3l2zrfmfldy22": "bat",
+  "3l2zrfayyxo2p": "blood-drop",
+  "3l2zrewoxky2s": "skull",
+  "3l2zrek5jzz22": "ghost",
+  "3l2zre5lg542s": "jack-o-lantern",
+  "3l2zrc72g2m2o": "coffe-cigarette",
+  "3l36g4nzj3g2k": "crocodile",
+  "3l36g546nxy2x": "frog",
+};
